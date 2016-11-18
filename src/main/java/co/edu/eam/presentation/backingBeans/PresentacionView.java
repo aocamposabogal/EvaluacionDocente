9e226818ae23fc6679314c30698057f0e1475022
danie <danie@DESKTOP-92V359R>
2016-11-17 23:30:36 -0500

    Change-Id: I8f186383abffb5fe10360826ffaf6c92f2e1319e

--- src/main/java/co/edu/eam/presentation/backingBeans/PresentacionView.java
+++ src/main/java/co/edu/eam/presentation/backingBeans/PresentacionView.java
@@ -104,13 +104,6 @@ public class PresentacionView implements Serializable {
 		}
 	}
 
-	public String action_new() {
-		selectedPresentacion = null;
-		setShowDialog(true);
-
-		return "";
-	}
-
 	public void listener_txtId() {
 		try {
 			Integer id = FacesUtils.checkInteger(txtId);
@@ -174,6 +167,7 @@ public class PresentacionView implements Serializable {
 	}
 
 	public String action_save() {
+		selectedPresentacion = null;
 		try {
 
 			inicializarPresentaciones();
