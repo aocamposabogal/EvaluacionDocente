f0f50128519d2f0a9c2876db40fa906f8d322f44
Alvaro021095 <lotero021095@gmail.com>
2016-11-17 22:56:39 -0500

    Metodos de consultar la informacion de un docente por la materia y consultar los docentes activos por programa.

---
+++ src/main/java/co/edu/eam/modelo/dto/DocenteDTO.java
@@ -0,0 +1,74 @@
+package co.edu.eam.modelo.dto;
+import java.io.Serializable;
+
+
+public class DocenteDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private long codigoPrograma;
+	private String nombrePrograma;
+	private String codigoDocente;
+	private String nombreDocente;
+	private String codigoMateria;
+	private String nombreMateria;
+	private String grupo;
+	private String jornada;
+	
+	
+	
+	
+	
+	public String getCodigoMateria() {
+		return codigoMateria;
+	}
+	public void setCodigoMateria(String codigoMateria) {
+		this.codigoMateria = codigoMateria;
+	}
+	public String getNombreMateria() {
+		return nombreMateria;
+	}
+	public void setNombreMateria(String nombreMateria) {
+		this.nombreMateria = nombreMateria;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+	public String getJornada() {
+		return jornada;
+	}
+	public void setJornada(String jornada) {
+		this.jornada = jornada;
+	}
+	public long getCodigoPrograma() {
+		return codigoPrograma;
+	}
+	public void setCodigoPrograma(long codigoPrograma) {
+		this.codigoPrograma = codigoPrograma;
+	}
+	public String getNombrePrograma() {
+		return nombrePrograma;
+	}
+	public void setNombrePrograma(String nombrePrograma) {
+		this.nombrePrograma = nombrePrograma;
+	}
+	public String getCodigoDocente() {
+		return codigoDocente;
+	}
+	public void setCodigoDocente(String codigoDocente) {
+		this.codigoDocente = codigoDocente;
+	}
+	public String getNombreDocente() {
+		return nombreDocente;
+	}
+	public void setNombreDocente(String nombreDocente) {
+		this.nombreDocente = nombreDocente;
+	}
+	
+	
+	
+	
+}
--- src/main/java/co/edu/eam/ws/WebServiceClient.java
+++ src/main/java/co/edu/eam/ws/WebServiceClient.java
@@ -16,12 +16,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import co.edu.eam.modelo.dto.MateriaDTO;
-
+import co.edu.eam.modelo.dto.DocenteDTO;
 public class WebServiceClient {
 
-	JSONArray finalResult = null;
+	static JSONArray finalResult = null;
+	
+	
 
-	private JSONArray invocarServicioMateriasPorCodigo(String codigo) {
+	public static JSONArray invocarServicioMateriasPorCodigo(String codigo, String combo) {
 
 		DefaultHttpClient httpClient = new DefaultHttpClient();
 		try {
@@ -29,7 +31,9 @@ public class WebServiceClient {
 			HttpPost request = new HttpPost("http://www.eam.edu.co/service/servicio.php");
 			request.addHeader("accept", "application/json");
 			List<BasicNameValuePair> nvp = new ArrayList<BasicNameValuePair>();
-			nvp.add(new BasicNameValuePair("codig", "2706"));
+			nvp.add(new BasicNameValuePair("codig", codigo));
+			nvp.add(new BasicNameValuePair("signal", combo));
+			//
 			request.setEntity(new UrlEncodedFormEntity(nvp, HTTP.UTF_8));
 			HttpResponse response = httpClient.execute(request);
 
@@ -50,18 +54,19 @@ public class WebServiceClient {
 		return finalResult;
 	}
 	
+	
 	/**
 	 * 
 	 * @param codigo Codigo docente
 	 * @return Listado de materias por codigo docente
 	 */
 
-	public List<MateriaDTO> consultarMateriasPorCodigoDocente(String codigo) {
+	public static List<MateriaDTO> consultarMateriasPorCodigoDocente(String codigo) {
 
 		ArrayList<MateriaDTO> listadoMaterias = new ArrayList<>();
 		
 		//se llena las lista con todos las materias en un objeto de tipo httpClient
-		invocarServicioMateriasPorCodigo(codigo);
+		invocarServicioMateriasPorCodigo(codigo,"1");
 		
 		// se separan las materias una a una y se agregan a una lista en un objeto tipo MateriaDTO
 		@SuppressWarnings("rawtypes")
@@ -87,7 +92,7 @@ public class WebServiceClient {
 		ArrayList<MateriaDTO> listadoMaterias = new ArrayList<>();
 		
 		//se llena las lista con todos las materias en un objeto de tipo httpClient
-		invocarServicioMateriasPorCodigo(codigo);
+		invocarServicioMateriasPorCodigo(codigo,"1");
 		
 		// se separan las materias una a una y se agregan a una lista en un objeto tipo MateriaDTO
 		@SuppressWarnings("rawtypes")
@@ -106,6 +111,95 @@ public class WebServiceClient {
 		
 		return listadoMaterias;
 
+	}
+	
+	/**
+	 * Metodo para conocer los datos de los profesor activos por programa
+	 * @param codigo, codigo de la materia
+	 * @param combo, numero que se obtiene en el combo para filtrar la busqueda
+	 * @return, retorna los docentes activos del programa.
+	 */
+	public static List<DocenteDTO> consultarDocentesActivosPorPrograma(String codigo, String combo) {
+
+		ArrayList<DocenteDTO> listadoDocente = new ArrayList<>();
+		
+		//se llena las lista con todos los docentes en un objeto de tipo httpClient
+		invocarServicioMateriasPorCodigo(codigo,"3");
+		
+		// se separan los docentes uno a uno y se agregan a una lista en un objeto tipo DocenteDTO
+		@SuppressWarnings("rawtypes")
+		Iterator i = finalResult.iterator();
+		while (i.hasNext()) {
+			JSONObject result = (JSONObject) i.next();
+			DocenteDTO doc = new DocenteDTO();
+			doc.setCodigoDocente((String)result.get("codigoprofe"));
+			doc.setCodigoPrograma((long)result.get("programa"));
+			doc.setNombreDocente((String)result.get("nomcompleto"));
+			doc.setNombrePrograma((String)result.get("nprograma"));
+		
+			listadoDocente.add(doc);
+		}
+		
+		return listadoDocente;
+
+	}
+	
+	/**
+	 * Metodo para obtener los datos de un docente consultando la materia.
+	 * @param codigo, Codigo de la materia.
+	 * @param combo, Numero que se obtiene al filtrar la busqueda.
+	 * @return, Retorna los datos del docente o los docentes.
+	 */
+	public static List<DocenteDTO> consultarDocentesPorMateria(String codigo, String combo) {
+
+		ArrayList<DocenteDTO> listadoDocente = new ArrayList<>();
+		
+		//se llena las lista con todos las materias en un objeto de tipo httpClient
+		invocarServicioMateriasPorCodigo(codigo,"2");
+		
+		// se separan los docentes uno a uno y se agregan a una lista en un objeto tipo DocenteDTO
+		@SuppressWarnings("rawtypes")
+		Iterator i = finalResult.iterator();
+		while (i.hasNext()) {
+			JSONObject result = (JSONObject) i.next();
+			DocenteDTO doc = new DocenteDTO();
+			doc.setCodigoMateria((String)result.get("espacio"));
+			doc.setNombreMateria((String)result.get("nespacio"));
+			doc.setCodigoDocente((String)result.get("codigoprofe"));
+			doc.setNombreDocente((String)result.get("nomcompleto"));
+			doc.setGrupo((String)result.get("grupo"));
+			doc.setJornada((String)result.get("jornada"));
+		
+			listadoDocente.add(doc);
+		}
+		
+		return listadoDocente;
+
+	}
+	
+	public static void main(String[] args){
+		/*
+		List<MateriaDTO> materias=consultarMateriasPorCodigoDocente("2706");
+		for (MateriaDTO materiaDTO : materias) {
+			System.out.println(materiaDTO.getNombre());
+		}
+		*/
+		
+		/*
+		List<DocenteDTO> doc= consultarDocentesActivosPorPrograma("23","3");
+		for (DocenteDTO docts : doc) {
+			System.out.println(docts.getCodigoDocente()+","+ docts.getNombreDocente());
+		}
+		*/
+		
+		/*
+		List<DocenteDTO> doc= consultarDocentesPorMateria("20755", "2");
+		for (DocenteDTO docts : doc) {
+			System.out.println(docts.getCodigoDocente()+","+ docts.getNombreDocente());
+		}
+		*/
+		
+		
 	}
 
 }
